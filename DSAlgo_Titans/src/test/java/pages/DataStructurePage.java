5e92f9fce432328bd105450f55db33e3e7ecfc4f
aruna6002 <arunabr6002@gmail.com>
2024-08-22 12:59:29 -0700

    Commiting new files

---
+++ DSAlgo_Titans/src/test/java/pages/DataStructurePage.java
@@ -0,0 +1,10 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class DataStructurePage {
+@FindBy(xpath="/html/body/div[3]/div[1]/div/div/a") WebElement Getstartedbutton;
+
+
+}
--- DSAlgo_Titans/src/test/java/stepdeffination/ArrayStep.java
+++
@@ -1,5 +0,0 @@
-package stepdeffination;
-
-public class ArrayStep {
-
-}
