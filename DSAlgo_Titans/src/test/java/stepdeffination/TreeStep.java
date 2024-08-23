c29e792607dd0ecca7bfe28600f639ff9ecadb97
sulakshana763 <sulakshana763@gmail.com>
2024-08-23 11:27:53 -0700

    Tree module files with test cases added

---
+++ DSAlgo_Titans/src/test/java/stepdeffination/TreeStep.java
@@ -0,0 +1,285 @@
+package stepdeffination;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class TreeStep {
+	
+	@Given("User launch Chrome browser")
+	public void user_launch_chrome_browser() {
+		
+	}
+
+	@When("User opens URL {string}")
+	public void user_opens_url(String url) {
+		
+	}
+
+	@When("Click on Sign in")
+	public void click_on_sign_in() {
+		
+	}
+
+	@When("User enters Username as {string} and Password as {string}")
+	public void user_enters_username_as_and_password_as(String username, String password) {
+		
+	}
+
+	@When("Click on Login")
+	public void click_on_login() {
+		
+	}
+
+
+	@Then("User can view the Numpy Ninja Homepage")
+	public void user_can_view_the_numpy_ninja_homepage() {
+	    
+	}
+
+	@When("User click on Get Started under Tree block")
+	public void user_click_on_get_started_under_tree_block() {
+		 
+	    
+	}
+
+	@Then("user can view Tree page")
+	public void user_can_view_tree_page() {
+	   
+	    
+	}
+
+	@When("User click on Overview of Trees")
+	public void user_click_on_overview_of_trees() {
+		 
+	    
+	}
+
+	@Then("User can view Overview of Trees page")
+	public void user_can_view_overview_of_trees_page() {
+	    
+	    
+	}
+
+	@When("User click on Try here")
+	public void user_click_on_try_here() {
+	    
+	
+	}
+
+	@Then("User can view text editor")
+	public void user_can_view_text_editor() {
+	    
+	    
+	}
+
+	@When("User write some valid python code")
+	public void user_write_some_valid_python_code() {
+	    
+	    
+	}
+
+	@When("click Run")
+	public void click_run() {
+	    
+	    
+	}
+
+	@Then("User can view message box")
+	public void user_can_view_message_box() {
+	    
+	    
+	}
+
+	@When("user enters the required value")
+	public void user_enters_the_required_value() {
+	    
+	    
+	}
+
+	@When("clicks Ok")
+	public void clicks_ok() {
+	    
+	    
+	}
+
+	@Then("User can view the result of the python code")
+	public void user_can_view_the_result_of_the_python_code() {
+	    
+	    
+	}
+
+	@Then("close browser")
+	public void close_browser() {
+	    
+	    
+	}
+
+	@When("User click on Overview of Tree")
+	public void user_click_on_overview_of_tree() {
+	    
+	    
+	}
+
+	@Then("User can view Overview of Tree page")
+	public void user_can_view_overview_of_tree_page() {
+	    
+	    
+	}
+
+	@When("User click on Terminologies")
+	public void user_click_on_terminologies() {
+	    
+	    
+	}
+
+	@Then("User can view Terminologies page")
+	public void user_can_view_terminologies_page() {
+	    
+	    
+	}
+
+	@When("User click on Types of Trees")
+	public void user_click_on_types_of_trees() {
+	    
+	    
+	}
+
+	@Then("User can view Types of Trees page")
+	public void user_can_view_types_of_trees_page() {
+	    
+	    
+	}
+
+	@When("User click on Tree Traversals")
+	public void user_click_on_tree_traversals() {
+	    
+	    
+	}
+
+	@Then("User can view Tree Traversals page")
+	public void user_can_view_tree_traversals_page() {
+	    
+	    
+	}
+
+	@When("User click on Traversals Illustration")
+	public void user_click_on_traversals_illustration() {
+	    
+	    
+	}
+
+	@Then("User can view Traversals Illustration page")
+	public void user_can_view_traversals_illustration_page() {
+	    
+	    
+	}
+
+	@When("User click on Binary Trees")
+	public void user_click_on_binary_trees() {
+	    
+	    
+	}
+
+	@Then("User can view Binary Trees page")
+	public void user_can_view_binary_trees_page() {
+	    
+	    
+	}
+
+	@When("User click on Types of Binary Trees")
+	public void user_click_on_types_of_binary_trees() {
+	    
+	    
+	}
+
+	@Then("User can view Types of Binary Trees page")
+	public void user_can_view_types_of_binary_trees_page() {
+	    
+	    
+	}
+
+	@When("User click on Implementation in Python")
+	public void user_click_on_implementation_in_python() {
+	    
+	    
+	}
+
+	@Then("User can view Implementation in Python page")
+	public void user_can_view_implementation_in_python_page() {
+	    
+	    
+	}
+
+	@When("User click on Binary Tree Traversals")
+	public void user_click_on_binary_tree_traversals() {
+	    
+	    
+	}
+
+	@Then("User can view Binary Tree Traversals page")
+	public void user_can_view_binary_tree_traversals_page() {
+	    
+	    
+	}
+
+	@When("User click on Implementation of Binary Trees")
+	public void user_click_on_implementation_of_binary_trees() {
+	    
+	    
+	}
+
+	@Then("User can view Implementation of Binary Trees page")
+	public void user_can_view_implementation_of_binary_trees_page() {
+	    
+	    
+	}
+
+	@When("User click on Application of Binary Trees")
+	public void user_click_on_application_of_binary_trees() {
+	    
+	    
+	}
+
+	@Then("User can view Application of Binary Trees page")
+	public void user_can_view_application_of_binary_trees_page() {
+	    
+	    
+	}
+
+	@When("User click on Binary Search Trees")
+	public void user_click_on_binary_search_trees() {
+	    
+	    
+	}
+
+	@Then("User can view Binary Search Trees page")
+	public void user_can_view_binary_search_trees_page() {
+	    
+	    
+	}
+
+	@When("User click on Implementation of BSTs")
+	public void user_click_on_implementation_of_bs_ts() {
+	    
+	    
+	}
+
+	@Then("User can view Implementation of BSTs page")
+	public void user_can_view_implementation_of_bs_ts_page() {
+	    
+	    
+	}
+
+	@When("User click on Implementation of BST")
+	public void user_click_on_implementation_of_bst() {
+	    
+	    
+	}
+
+	@Then("User can view Implementation of BST page")
+	public void user_can_view_implementation_of_bst_page() {
+	    
+	    
+	}
+
+}
